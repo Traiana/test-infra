3e0b4a15edec5cc182abe5db8623a0a644c42ef8
Regevd <40140718+Regevd@users.noreply.github.com>
2020-07-07 10:06:03 +0300

    add return value to CreateTag

--- prow/github/client.go
+++ prow/github/client.go
@@ -136,7 +136,7 @@ type CommitClient interface {
 	GetRef(org, repo, ref string) (string, error)
 	DeleteRef(org, repo, ref string) error
 	CreateRef(org, repo, ref, SHA string) error
-	CreateTag(org, repo, tag, massage, SHA, objType string, tagger Tagger) error
+	CreateTag(org, repo, tag, message, SHA, objType string, tagger Tagger) (string, error)
 }
 
 // RepositoryClient interface for repository related API actions
@@ -2407,10 +2407,18 @@ func (c *client) GetRef(org, repo, ref string) (string, error) {
 // See https://developer.github.com/v3/git/refs/#create-a-reference
 func (c *client) CreateRef(org, repo, ref, SHA string) error {
 	c.log("CreateRef", org, repo)
+	body := struct {
+		Ref string `json:"ref"`
+		SHA string `json:"sha"`
+	}{
+		Ref: ref,
+		SHA: SHA,
+	}
 	_, err := c.request(&request{
 		method:    http.MethodPost,
 		path:      fmt.Sprintf("/repos/%s/%s/git/refs", org, repo),
 		exitCodes: []int{201},
+		requestBody: body,
 	}, nil)
 	return err
 }
@@ -2418,29 +2426,32 @@ func (c *client) CreateRef(org, repo, ref, SHA string) error {
 // CreateRef returns the SHA of the given ref, such as "heads/master".
 //
 // See https://developer.github.com/v3/git/tags/#create-a-tag-object
-func (c *client) CreateTag(org, repo, tag, massage, SHA, objType string, tagger Tagger) error {
+func (c *client) CreateTag(org, repo, tag, message, SHA, objType string, tagger Tagger) (string, error) {
 	body := struct {
-		Tag string `json:"tag"`
-		Massage string `json:"massage"`
-		Object string `json:"object"`
-		Type string `json:"type"`
-		Tagger Tagger `json:"tagger"`
+		Tag     string `json:"tag"`
+		Message string `json:"message"`
+		Object  string `json:"object"`
+		Type    string `json:"type"`
+		Tagger  Tagger `json:"tagger"`
 	}{
 		Tag:     tag,
-		Massage: massage,
+		Message: message,
 		Object:  SHA,
 		Type:    objType,
 		Tagger:  tagger,
 	}
 
+	var resp struct {
+		SHA string `json:"sha"`
+	}
 	c.log("CreateTag", org, repo)
 	_, err := c.request(&request{
-		method:    http.MethodPost,
-		path:      fmt.Sprintf("/repos/%s/%s/git/tags", org, repo),
-		exitCodes: []int{201},
+		method:      http.MethodPost,
+		path:        fmt.Sprintf("/repos/%s/%s/git/tags", org, repo),
+		exitCodes:   []int{201},
 		requestBody: body,
-	}, nil)
-	return err
+	}, &resp)
+	return resp.SHA, err
 }
 
 // DeleteRef deletes the given ref
